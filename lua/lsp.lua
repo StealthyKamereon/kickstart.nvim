f457fb3e706241b991b1fa1210387be249d3da00
Maël Godey <30075012+StealthyKamereon@users.noreply.github.com>
2024-10-24 08:38:34 +0200

    fix: handled nil case

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -78,7 +78,9 @@ local servers = {
 -- merge two nested tables, in place
 function merge(t1, t2)
   for k, v in pairs(t2) do
-    if type(v) == 'table' then
+    if t1[k] == nil then
+      t1[k] = v
+    elseif type(v) == 'table' then
       t1[k] = merge(t1[k], t2[k])
     else
       t1[k] = v
